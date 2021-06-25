4bbe385dcc4b50bff13dc7d9fe490aca96b34031
Sophie Kopaliani <sophie.kopaliani@gmail.com>
2021-06-25 21:12:10 +0400

    Initial Commit

---
+++ MyMoviesApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyMoviesApp
+//
+//  Created by Sophie Kopaliani on 6/25/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
